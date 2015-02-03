d86a164785b5248936c4a2b02100bdd8a9174a67
David Campbell <dcampbell24@gmail.com>
2015-02-02 19:59:03 -0500

    Replace bool returns with IupResult.

--- src/iup/lib.rs
+++ src/iup/lib.rs
@@ -1,3 +1,11 @@
+//! Rust high level bindings for [IUP][1] -- A Portable User Interface Toolkit
+//!
+//! [1]: http://www.tecgraf.puc-rio.br/iup/
+//!
+//! The functions in this crate are a thin wrapper around the IUP functions unless noted
+//! otherwise. Refer to the IUP website for their full documentation.
+//!
+
 extern crate libc;
 extern crate "iup-sys" as sys;
 
@@ -6,12 +14,23 @@ use std::ptr;
 use sys::Ihandle;
 use libc::{c_char, c_int};
 
-// FIXME: Properly pass argc and argv to IupOpen.
-pub fn open() -> bool {
+pub type IupResult = Result<(), String>;
+
+/// Initializes the IUP toolkit. Must be called before any other IUP function.
+pub fn open() -> IupResult {
     let p1 : *const c_int = ptr::null();
     let p2 : *const *const *const c_char = ptr::null();
-    let ok = unsafe { sys::IupOpen(p1, p2) };
-    ok == sys::IUP_NOERROR
+
+    match unsafe { sys::IupOpen(p1, p2) } {
+        sys::IUP_NOERROR => Ok(()),
+        sys::IUP_OPENED => Err("IUP_OPENED: iup::open called while already open.".to_string()),
+        sys::IUP_ERROR => Err("IUP_ERROR: X-Windows is not initialized".to_string()),
+        _ => unreachable!(),
+    }
+}
+
+pub fn close() {
+    unsafe { sys::IupClose(); }
 }
 
 pub fn label(s: &str) -> &mut Ihandle {
@@ -19,19 +38,19 @@ pub fn label(s: &str) -> &mut Ihandle {
     unsafe { &mut *(sys::IupLabel(c_string.as_ptr())) } 
 }
 
-pub fn close() {
-    unsafe { sys::IupClose(); }
-}
-
-pub fn main_loop() -> bool {
-    unsafe { sys::IupMainLoop() == sys::IUP_NOERROR }
+pub fn main_loop() {
+    let ok = unsafe { sys::IupMainLoop() };
+    assert_eq!(ok, sys::IUP_NOERROR);
 }
 
 pub fn dialog(ih: &mut Ihandle) -> &mut Ihandle {
     unsafe { &mut *(sys::IupDialog(ih)) }
 }
 
-pub fn show(ih: &mut Ihandle) -> bool {
-    let ok = unsafe { sys::IupShow(ih) };
-    ok == sys::IUP_NOERROR
+pub fn show(ih: &mut Ihandle) -> IupResult {
+    match unsafe { sys::IupShow(ih) } {
+        sys::IUP_NOERROR => Ok(()),
+        sys::IUP_ERROR => Err("IUP_ERROR: unknown error".to_string()),
+        _ => unreachable!(),
+    }
 }
