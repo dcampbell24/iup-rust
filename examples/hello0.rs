aaead324c407c592ad63935d5a1f37c00b0deac8
David Campbell <dcampbell24@gmail.com>
2015-02-05 20:41:35 -0500

    Update hello_world

--- examples/hello0.rs
+++ examples/hello0.rs
@@ -1,11 +1,11 @@
+//! Example based on hello0 from http://wiki.call-cc.org/iup-tutor
+
 extern crate iup;
 
 fn main() {
     iup::open().unwrap();
-
     let mut dialog = iup::dialog(iup::label("Hello, world!"));
     iup::show(&mut dialog).unwrap();
-
     iup::main_loop();
     iup::close();
 }
