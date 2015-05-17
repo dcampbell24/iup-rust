b0d1992afe5d4530bd93bea7cacfaf770d0f97c7
David Campbell <dcampbell24@gmail.com>
2015-05-17 11:16:14 -0400

    counter_newcb.rs -> counter.rs, make more like 7gui example

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,39 +1,38 @@
 extern crate iup;
 
 use iup::CallbackReturn;
-
-extern fn button_1_cb(_ih: *mut iup::IhandleRaw) -> CallbackReturn {
-    let mut text = iup::get_handle("text").unwrap();
-    let count = iup::get_attribute(&mut text, "VALUE").unwrap().parse::<i32>().unwrap();
-    iup::set_str_attribute(&mut text, "VALUE", &(count + 1).to_string());
-    CallbackReturn::Default
-}
-
-extern fn button_2_cb(_ih: *mut iup::IhandleRaw) -> CallbackReturn {
-    let mut text = iup::get_handle("text").unwrap();
-    let count = iup::get_attribute(&mut text, "VALUE").unwrap().parse::<i32>().unwrap();
-    iup::set_str_attribute(&mut text, "VALUE", &(count - 1).to_string());
-    CallbackReturn::Default
-}
+use std::cell::RefCell;
+use std::rc::Rc;
 
 fn main() {
     iup::open().unwrap();
 
-    let label = iup::label("Counter");
+    let text = Rc::new(RefCell::new(iup::text()));
+    {
+        let mut text = text.borrow_mut();
+        iup::set_handle("text", &mut text);
+        iup::set_str_attribute(&mut text, "READONLY", "YES");
+        iup::set_str_attribute(&mut text, "VALUE", "0");
+    }
 
-    let mut text = iup::text();
-    iup::set_handle("text", &mut text);
-    iup::set_str_attribute(&mut text, "READONLY", "YES");
-    iup::set_str_attribute(&mut text, "VALUE", "0");
+    let mut button = iup::button("Count");
+    {
+        let text = text.clone();
+        iup::callback::set_action(&mut button, Some(move |_ih| {
+            let mut text = text.borrow_mut();
+            let count = iup::get_attribute(&mut text, "VALUE").unwrap().parse::<i32>().unwrap();
+            iup::set_str_attribute(&mut text, "VALUE", &(count + 1).to_string());
+            CallbackReturn::Default
+        }));
+        iup::callback::set_destroy_cb(&mut button, Some(|_ih|{
+            println!("One of the buttons is getting destroyed!!");
+        }));
+    }
 
-    let mut button_1 = iup::button("Count Up");
-    iup::set_callback(&mut button_1, "ACTION", button_1_cb);
-
-    let mut button_2 = iup::button("Count Down");
-    iup::set_callback(&mut button_2, "ACTION", button_2_cb);
-
-    let mut hbox = iup::hboxv(vec!(label, text, button_1, button_2));
+    let mut hbox = iup::hboxv(vec!(text.borrow().clone(), button));
     iup::set_str_attribute(&mut hbox, "ALIGNMENT", "ACENTER");
+    iup::set_str_attribute(&mut hbox, "MARGIN", "10x10");
+    iup::set_str_attribute(&mut hbox, "GAP", "10");
 
     iup::show(&mut iup::dialog(hbox)).unwrap();
 
