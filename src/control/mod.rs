e2011fb1625d8f9607bec028efa120eeccbc44e9
Denilson M. Amorim <dma_2012@hotmail.com>
2015-05-31 23:35:29 -0300

    Toggle #13

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -5,8 +5,11 @@ pub mod label;
 pub mod text;
 pub mod button;
 pub mod progress;
+pub mod toggle;
 
 pub use self::text::{Text, TextAction};
 pub use self::label::Label;
 pub use self::button::Button;
 pub use self::progress::ProgressBar;
+pub use self::toggle::{Toggle, ToggleAction};
+
---
+++ src/control/toggle.rs
@@ -0,0 +1,54 @@
+// TODO MOD DOC
+use iup_sys;
+use std::ptr;
+use std::ffi::CString;
+
+use Element;
+
+/// See the [IUP Toggle Documentation][1].
+/// [1]: http://webserver2.tecgraf.puc-rio.br/iup/en/elem/iuptoggle.html
+pub struct Toggle(*mut iup_sys::Ihandle);
+
+impl Toggle {
+    /// Creates a toggle with no text.
+    pub fn new() -> Toggle {
+        unsafe { Toggle::from_raw(iup_sys::IupToggle(ptr::null_mut(), ptr::null_mut())) }
+    }
+
+    /// Creates a toggle with the specified text.
+    pub fn with_title<S: Into<String>>(title: S) -> Toggle {
+        let ctitle = CString::new(title.into()).unwrap();
+        unsafe { Toggle::from_raw(iup_sys::IupToggle(ctitle.as_ptr(), ptr::null_mut())) }
+    }
+}
+
+impl_element!(Toggle, "toggle");
+impl ::callback::MapCb for Toggle {}
+impl ::callback::UnmapCb for Toggle {}
+impl ::callback::GetFocusCb for Toggle {}
+impl ::callback::KillFocusCb for Toggle {}
+impl ::callback::EnterWindowCb for Toggle {}
+impl ::callback::LeaveWindowCb for Toggle {}
+impl ::callback::HelpCb for Toggle {}
+// TODO impl K_ callbacks when it's implemented.
+
+/// Called after the value was interactively changed by the user.
+///
+/// Called after the ACTION callback, but under the same context.
+impl ::callback::ValueChangedCb for Toggle {}
+
+/// See the `ToggleAction` documentation.
+impl self::ToggleAction for Toggle {}
+impl_callback! {
+	/// Action generated when the toggle's state (on/off) was changed.
+	///
+	/// The callback boolean parameter represents the state the toggle was switched to.
+	///
+	/// `CallbackReturn::Close` will be processed
+    pub trait ToggleAction where Self: Element {
+        let name = "ACTION";
+        extern fn listener(ih: *mut iup_sys::Ihandle, state: c_int) -> CallbackReturn;
+        fn set_action<F: Callback(Self, bool)>(&mut self, cb: F) -> Self;
+        fn remove_action(&mut self) -> Option<Box<_>>;
+    }
+}
